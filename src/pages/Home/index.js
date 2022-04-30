94acc75a7a05d84e028f23f6d793a2e201eb4d13
vlucena <lucenavictoria0@gmail.com>
2022-04-30 03:06:42 -0300

    tranformando filme em array e mapeando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,42 @@
 import { Container, Movie, MovieList } from './style'
 
 function Home() {
+  const movies = [
+    {
+      title: 'A proposta',
+      image_url:
+        'https://br.web.img3.acsta.net/medias/nmedia/18/87/29/75/19874043.jpg'
+    },
+
+    {
+      title: 'Sherek',
+      image_url:
+        'https://upload.wikimedia.org/wikipedia/pt/e/e6/Shrek_Poster.jpg'
+    },
+
+    {
+      title: 'Jonh Wick',
+      image_url:
+        'https://upload.wikimedia.org/wikipedia/pt/thumb/1/13/John_wick_ver3.jpg/250px-John_wick_ver3.jpg'
+    }
+  ]
+
   return (
     <Container>
       <h1>Movies</h1>
       <MovieList>
-        <Movie>
-          <a href="https://google.com.br">
-            <img
-              src="https://br.web.img3.acsta.net/medias/nmedia/18/87/29/75/19874043.jpg"
-              alt="a proposta"
-            />
-          </a>
-          <span>A proposta </span>
-        </Movie>
-        <Movie>
-          <a href="https://google.com.br">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/pt/e/e6/Shrek_Poster.jpg"
-              alt="Sherk"
-            />
-          </a>
-          <span>Sherk</span>
-        </Movie>
-        <Movie>
-          <a href="https://google.com.br">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/pt/thumb/1/13/John_wick_ver3.jpg/250px-John_wick_ver3.jpg"
-              alt="jonh wick"
-            />
-          </a>
-          <span>Jonh Wick</span>
-        </Movie>
+        {movies.map(movie => {
+          return (
+            <Movie>
+              <a href="https://google.com.br">
+                <img src={movie.image_url} alt={movie.title} />
+              </a>
+              <span>{movie.title}</span>
+            </Movie>
+          )
+        })}
       </MovieList>
     </Container>
   )
 }
-
 export default Home
